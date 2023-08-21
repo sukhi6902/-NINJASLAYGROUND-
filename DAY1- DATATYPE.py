4416b82fcc9c806a6c3f0866ad5f5ed7e5edb0f3
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-08-21 12:06:17 +0530

    Update DAY1- DATATYPE.py

--- DAY1- DATATYPE.py
+++ DAY1- DATATYPE.py
@@ -4,3 +4,7 @@ def dataTypes(type: str):
     dicti={"Integer":4,"Long":8,"Float":4,"Double":8,"Character":1}
     return dicti[type]
     pass
+
+
+
+https://www.codingninjas.com/studio/problems/data-type_9065109?challengeSlug=ninja-slayground&leftPanelTab=0
