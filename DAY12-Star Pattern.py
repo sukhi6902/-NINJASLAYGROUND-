9993447cf30f4b1a8b77efe812f8cfed85fa2ca3
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-08-24 10:41:28 +0530

    Create DAY12-Star Pattern.py

---
+++ DAY12-Star Pattern.py
@@ -0,0 +1,17 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+
+def printPattern(n):
+    k=1
+
+    for i in range(1,n+1):
+
+        print(" "*(n-i)+"*"*k)
+
+        k+=2
+
+
+https://www.codingninjas.com/studio/problems/star-pattern_9065120?challengeSlug=ninja-slayground&leftPanelTab=1
