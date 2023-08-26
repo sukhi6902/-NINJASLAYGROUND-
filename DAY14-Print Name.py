cf8d91bf4b4775237e99be8eb360e888b4123bd6
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-08-26 14:06:06 +0530

    Create DAY14-Print Name.py

---
+++ DAY14-Print Name.py
@@ -0,0 +1,8 @@
+def printName(s):
+
+    for i in range(0, 5):
+
+        print(s)
+
+https://www.codingninjas.com/studio/problems/print-name_9065122?challengeSlug=ninja-slayground&leftPanelTab=0
+ 
