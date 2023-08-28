fa4754d7e68a05e1ce1d257baca6046916ce783c
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-08-28 15:18:29 +0530

    Create DAY16-Largest Element in the Array.py

---
+++ DAY16-Largest Element in the Array.py
@@ -0,0 +1,9 @@
+from sys import *
+from collections import *
+from math import *
+
+def largestElement(arr: [], n: int) -> int:
+    arr.sort()
+    return arr[n-1]
+
+https://www.codingninjas.com/studio/problems/largest-element-in-the-array_9065124?challengeSlug=ninja-slayground&leftPanelTab=0
