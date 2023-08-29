7a6679eab4296cda150784aa36d66c173e243e77
vibharao <82702791+sukhi6902@users.noreply.github.com>
2023-08-29 07:27:15 +0530

    Create DAY16-Linear Search.java

---
+++ DAY16-Linear Search.java
@@ -0,0 +1,43 @@
+import java.util.*;
+
+public class Solution {
+
+    public static int linearSearch(int n, int num, int []arr){
+
+        // Write your code here.
+
+        for(int i=0;i<n;i++){
+
+            if(arr[i]==num){
+
+                return i;
+
+            }
+
+        }
+
+        return -1;
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+https://www.codingninjas.com/studio/problems/linear-search_9065125?challengeSlug=ninja-slayground
